4a0199ab7e27bce09106b26028f05863a17f80fe
zxs <zxspopo163@163.com>
2015-06-20 10:02:09 +0800

    增加shiro redis

---
+++ shiroRelation/src/main/java/com/jee/shiro/sessionDAO/RedisSessionDAO.java
@@ -0,0 +1,49 @@
+package com.jee.shiro.sessionDAO;
+
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.UnknownSessionException;
+import org.apache.shiro.session.mgt.eis.AbstractSessionDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+/**
+ * 整合分布式部署的session
+ * 使用redis作为缓存
+ */
+@Component("redisSessionDAO")
+public class RedisSessionDAO extends AbstractSessionDAO {
+
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Override
+    protected Serializable doCreate(Session session) {
+        return null;
+    }
+
+    @Override
+    protected Session doReadSession(Serializable serializable) {
+        return null;
+    }
+
+    @Override
+    public void update(Session session) throws UnknownSessionException {
+
+    }
+
+    @Override
+    public void delete(Session session) {
+
+    }
+
+    @Override
+    public Collection<Session> getActiveSessions() {
+        return null;
+    }
+}
