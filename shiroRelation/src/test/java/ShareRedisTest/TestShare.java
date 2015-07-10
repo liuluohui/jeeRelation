5a4c4c942be9c1a62131980154bfc0672a18ace2
zxs <zxspopo163@163.com>
2015-07-10 21:39:17 +0800

    增加redisSessionDAO实现。

--- shiroRelation/src/test/java/ShareRedisTest/TestShare.java
+++ shiroRelation/src/test/java/ShareRedisTest/TestShare.java
@@ -1,42 +1,33 @@
 package ShareRedisTest;
 
+import com.jee.shiro.sessionDAO.RedisSessionDAO;
+import org.apache.shiro.session.mgt.SimpleSession;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-import redis.clients.jedis.ShardedJedis;
-import redis.clients.jedis.ShardedJedisPool;
-import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Administrator on 2015/7/9.
  */
-@ContextConfiguration(locations = "/spring-redis.xml")
+@ContextConfiguration(locations = {"/applicationContext.xml", "/spring-redis.xml"})
 public class TestShare extends AbstractJUnit4SpringContextTests {
 
     @Autowired
-    private ShardedJedisPool pool;
+    private RedisSessionDAO sessionDAO;
 
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
 
     @Test
-    public void test() {
-
-        // 从池中获取一个Jedis对象
-        ShardedJedis jedis = pool.getResource();
-        String keys = "name";
-        String value = "snowolf";
-        // 删数据
-        jedis.del(keys);
-        // 存数据
-        jedis.set(keys, value);
-        // 取数据
-        String v = jedis.get(keys);
-
-        System.out.println(v);
-
-        // 释放对象池
-        pool.returnResource(jedis);
-
-        assertEquals(value, v);
+    public void testSave() {
+//        sessionDAO.create(new SimpleSession());
+//        redisTemplate.boundValueOps("a").set("v");
+        System.out.println(redisTemplate.boundValueOps("a").get());
+        System.out.println(redisTemplate.boundHashOps("shiro_redis_session").get("de670761-8702-4531-be10-f082b203d274"));
     }
+
+
+
 }
